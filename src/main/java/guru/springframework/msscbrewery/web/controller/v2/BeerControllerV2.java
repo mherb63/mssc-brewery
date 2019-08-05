cf76c0d877f05782e89a17a4b4f070f46821cefb
Michael Herb <mherb@ciena.com>
2019-08-05 13:40:46 -0400

    add error handling on invalid BeerDto object

--- src/main/java/guru/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -4,10 +4,15 @@ import guru.springframework.msscbrewery.service.v2.BeerServiceV2;
 import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -26,15 +31,15 @@ public class BeerControllerV2 {
     }
 
     @PostMapping
-    public ResponseEntity createBeer(RequestEntity request, @RequestBody BeerDtoV2 beerDto) {
+    public ResponseEntity createBeer(HttpServletRequest request, @Valid @RequestBody BeerDtoV2 beerDto) {
         BeerDtoV2 createdBeer = beerService.createBeer(beerDto);
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Location", request.getUrl() + "/" + createdBeer.getId().toString());
+        headers.add("Location", request.getRequestURI() + "/" + createdBeer.getId().toString());
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeer(@PathVariable("beerId") UUID beerId,  @RequestBody BeerDtoV2 beerDto) {
+    public ResponseEntity updateBeer(@PathVariable("beerId") UUID beerId,  @Valid @RequestBody BeerDtoV2 beerDto) {
         beerService.updateBeer(beerId, beerDto);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
@@ -43,6 +48,18 @@ public class BeerControllerV2 {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBeer (@PathVariable("beerId") UUID beerId) {
         beerService.deleteBeer(beerId);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List> validationErrorHandler(MethodArgumentNotValidException e) {
+        List<String> errors = new ArrayList<>(e.getBindingResult().getAllErrors().size());
+
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            errors.add("Field: " + ((FieldError) error).getField() + " : " + error.getDefaultMessage());
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
 }
